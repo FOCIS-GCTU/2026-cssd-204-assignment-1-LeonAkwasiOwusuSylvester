4b8aa02c53596c7ad5661012e12907377a75d07a
Leon Akwasi Sylvester Owusu <Leonakwasiowususylvester@gmail.com>
2026-05-29 15:21:11 +0000

    initials.py assigment one done
    Added a program to print initials in a stylized format.

--- initials.py
+++ initials.py
@@ -1,7 +1,27 @@
-
+#File: initial.py
+#Description: Print out my initials.
+#Assignment Nume: 1
+#
+#Name: Leon Akwasi Sylvester Owusu
+#STUDENT ID: 2425400583
+#Email:2425400583@live.gctu.edu.gh
+#Grader: Dr. Augustus Buckman
+#
+#On my honor, Leon Akwasi Sylvester Owusu, this programming assignment is my own work and I have not provided this code to any other student.
+print(' ')
 def main():
-  pass #code goes here
-
-
-
+ print('...LASO')
+ print('         ')
+ print('...LLL..........................AAAAA......................OOOOOOOOOOOo...................SSSSSSSSSSS.......')
+ print('...LLL.........................AA....AA...................OO...........OO................SSSSSSSSSS.......')
+ print('...LLL........................AA......AA.................OO.............OO................SSSS..............')
+ print('...LLL.......................AA.......AA.................OO.............OO................SSSS..............')
+ print('...LLL......................AA.........AA................OO.............OO.................SSSSSS..........')
+ print('...LLL.....................AAAAAAAAAAAAAAA...............OO.............OO...................SSSSSS........')
+ print('...LLL....................AA.............AA..............OO.............OO......................SSSSS......')
+ print('...LLL...................AA...............AA.............OO.............OO.......................SSSS......')
+ print('...LLLLLLLLLLLLL...**...AA.................AA...**.......OO.............OO...**...SSSSSSSSSSSSSSS........**')
+ print('...LLLLLLLLLLLLL...**...AA..................AA...**.........OOOOOOOOOOOOO.....**...SSSSSSSSSSSSSSS........**')
+ print()
 main()
+
